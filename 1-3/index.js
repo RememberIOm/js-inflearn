6e8ffc61b8645d0e36c2e3f6369321ad0fec02a0
RememberIOm <rememberiom@icloud.com>
2023-02-05 13:27:20 +0900

    feat: Add 1-3

---
+++ 1-3/index.js
@@ -0,0 +1,81 @@
+console.log('Hello,', '철수');
+
+// 값들을 주머니에 담아 사용하기
+const SALUTATION = 'Hello,';
+let person = '철수';
+
+console.log(SALUTATION, person);
+
+person = '영희';
+
+console.log(SALUTATION, person);
+
+
+// let x;
+// console.log(x);
+
+// x = 1;
+// console.log(x);
+
+
+// let x = 1;
+// console.log(x);
+
+
+// let x = 1;
+// let y = x; // x가 가진 값을 y에 할당
+
+// console.log(x, y);
+
+
+// let x = 1;
+// let y = x;
+// console.log('변경 전', x, y);
+
+// x = 'Hello!';
+// console.log('변경 후', x, y);
+
+
+// let x = 1;
+// console.log('첫 선언', x);
+
+// let x = 2;
+// console.log('다시 선언', x);
+
+
+// console.log(xyz);
+// let xyz = 1;
+
+
+
+// const PI = 3.14;
+// console.log('원주율:', PI);
+
+// const PI;
+// PI = 3.14;
+
+// const PI = 3.14;
+// PI = 3.14159
+
+
+// let a = 1;
+// let b = 2;
+// ...
+
+let a = 1, b = 2, c = 3;
+const X = 4, Y = 5, Z = 6;
+
+console.log(a, b, c);
+console.log(X, Y, Z);
+
+
+
+// const let = 1;
+
+// let typeof = 2;
+
+
+const 이름 = '홍길동';
+let 나이 = 20;
+
+console.log(이름, 나이);
