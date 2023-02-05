157ab7be6799de13639dfa7efaf8381da4348bb8
RememberIOm <rememberiom@icloud.com>
2023-02-05 13:04:05 +0900

    feat: Add 1-1

---
+++ 1-1/index.js
@@ -0,0 +1,12 @@
+console.log('Hello, world!');
+console.log(1);
+console.log(true);
+console.log({ name: '홍길동', age: 20, married: false });
+console.log('Hi!', 100, true, [1, 2, 3]);
+
+console.log(this);
+
+console.log('로그 - 기본적인 출력');
+console.info('로그 - 기능적으로는 log와 같음. 사용하기에 따라 용도 구분 가능');
+console.warn('경고 - 문제가 될 수 있는 부분');
+console.error('오류 - 에러 발생 상황');
